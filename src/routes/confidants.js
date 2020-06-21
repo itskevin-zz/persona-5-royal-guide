67e1ff426c72d4fe353b204faa53685ca5817e5c
Kevin Nguyen <test@email>
2020-06-21 00:20:55 -0400

    routes for confidants list page complete. Based confidants list view completed

--- src/routes/confidants.js
+++ src/routes/confidants.js
@@ -2,9 +2,21 @@ const express = require('express')
 const routes = new express.Router
 const fs = require('fs')
 
+const dataPath = './data/data.json'
+
+let data = { }
+fs.readFile(dataPath, (error, calendarData) => {
+    if (error) throw error
+    data = JSON.parse(calendarData)
+})
 
 routes.get('/confidants', (req, res) => {
-    res.send('list of confidants page')
+    const confidantsNameArray = data.confidants.map(confidant => confidant.name)
+    const confidantsArcanaArray = data.confidants.map(confidant => confidant.arcana)
+    const confidantsArray = confidantsNameArray.map((name, arcana) => name + " - " + confidantsArcanaArray[arcana])
+      res.render('confidants', {
+          confidants: confidantsArray
+      })
 })
 
 routes.get('/confidants/:id', (req, res) => {
