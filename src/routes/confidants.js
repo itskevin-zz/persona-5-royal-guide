e8bda8c23bdf2244ac1bf58d3244d11eed163bc5
kevin-knguyen <knguyen@west.com>
2020-06-22 23:16:39 -0400

    confidant view and routes completed

--- src/routes/confidants.js
+++ src/routes/confidants.js
@@ -11,31 +11,37 @@ fs.readFile(dataPath, (error, calendarData) => {
     data = JSON.parse(calendarData)
 })
 
-// let confidants = {}
-// fs.readFile(confidantsPath, (error, confidantsArray) => {
-//     if (error) throw error
-//     confidants = JSON.parse(confidantsArray)
-// })
-
-routes.get('/confidants', (req, res) => {
-    let confidantsName = []
-    for (let [key, value] of Object.entries(data.confidants)) {
-        confidantsName = value.name
-        console.log(confidantsName)
-    }
-
-    res.render('confidants', {
-        names: confidantsName
-    })
-
+let confidants = {}
+fs.readFile(confidantsPath, (error, confidantsList) => {
+    if (error) throw error
+    confidants = JSON.parse(confidantsList)
 })
 
-routes.get('/confidants/:id', (req, res) => {
-    const confidantName = req.params.id
+// store dates and confidants in object for page data. then cast in array for links/list using object.entries/keys/values
 
+routes.get('/confidants/:confidant', (req, res) => {
+    const confidantID = req.params.confidant
+
+    let confidantNames= []
+    for (let [key, value] of Object.entries(data.confidants)) {
+        confidantNames = Object.keys(data.confidants)
+    }
+    
     res.render('confidant', {
-        name: data.confidants.filter(confidant => confidant.name === 'Tae Takemi')
+        confidantName: data.confidants[confidantID].name,
+        confidantArcana: data.confidants[confidantID].arcana,
+        confidantLocation: data.confidants[confidantID].location,
+        confidantNotes: data.confidants[confidantID].notes,
+        confidantRanks: data.confidants[confidantID].ranks[0]
     })
+    
+})
+
+routes.get('/confidants', (req, res) => {
+    res.render('confidants',{
+        confidants: confidants.confidantsList
+    })
+    console.log(confidants.confidantsList)
 })
 
 module.exports = routes
